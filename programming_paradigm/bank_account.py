11bb2213a8f156664c71f05aede9911e6b45e534
Dev-MiMi <miracleolagundoye1@gmail.com>
2025-08-01 10:00:18 +0100

    my love

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,19 @@
+
+
+class BankAccount:
+    def __init__(self, account_balance, balance=0):
+        self.account_balance = account_balance
+        self.balance = balance
+    def deposit(self, amount):
+        return amount + self.account_balance
+    def withdraw(self, amount):
+        if self.account_balance >= amount:
+            return True
+        else:
+            return False
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance}")
+
+        
+
+
