6f578b1f273321d77c02d95dd8abcaedf9d4912c
Dev-MiMi <miracleolagundoye1@gmail.com>
2025-08-02 19:50:10 +0100

    okay

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,11 @@
+
+
+class Book:
+    def __init__(self, title, author, _is_checked_out):
+        self.title = title
+        self.author = author
+        self._is_checked_out = _is_checked_out
+class Library:
+    def __init__(self, _books):
+        self._books = _books
+
